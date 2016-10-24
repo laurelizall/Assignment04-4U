0bfba3f4b092c479be7007ab74f69415cfb72f45
halll7908 <halll7908@HRH-IC0027341.wrdsb.ca>
2016-10-24 14:24:24 -0400

    Assignment 4 - Progress.

---
+++ src/Game.java
@@ -0,0 +1,79 @@
+
+import java.awt.Color;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author halll7908
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // create the game board
+        Board board = new Board(12, 12);
+        
+        int random = (int) (Math.random() * 12);
+        
+        Doctor doc = new Doctor(random, random);
+        Dalek dalek1 = new Dalek(1, 1);
+        Dalek dalek2 = new Dalek(10, 10);
+        Dalek dalek3 = new Dalek(1, 10);
+
+        // Doctor inital position
+            board.putPeg(Color.GREEN, doc.getRow(), doc.getCol());
+        
+        // Daleks initial position
+        board.putPeg(Color.YELLOW, dalek1.getRow(), dalek1.getCol());
+        board.putPeg(Color.YELLOW, dalek2.getRow(), dalek2.getCol());
+        board.putPeg(Color.YELLOW, dalek3.getRow(), dalek3.getCol());
+
+        // message at the bottom
+        board.displayMessage("PLAY DALEKS, THE GAME");
+
+        while (true) {
+            // get a click on the board
+            Coordinate c = board.getClick();
+            int row = c.getRow();
+            int col = c.getCol();
+
+            // remove peg and place in new spot
+            board.removePeg(doc.getRow(), doc.getCol());
+            board.removePeg(dalek1.getRow(), dalek1.getCol());
+            board.removePeg(dalek2.getRow(), dalek2.getCol());
+            board.removePeg(dalek3.getRow(), dalek3.getCol());
+
+            doc.move(row, col);
+
+            dalek1.advanceTowards(doc);
+            dalek2.advanceTowards(doc);
+            dalek3.advanceTowards(doc);
+
+            // put peg at the click
+            board.putPeg(Color.GREEN, doc.getRow(), doc.getCol());
+
+            // Daleks
+            board.putPeg(Color.YELLOW, dalek1.getRow(), dalek1.getCol());
+            board.putPeg(Color.YELLOW, dalek2.getRow(), dalek2.getCol());
+            board.putPeg(Color.YELLOW, dalek3.getRow(), dalek3.getCol());
+            
+            // hasCrashed
+            if(dalek1.hasCrashed() == true){
+                board.putPeg(Color.RED, dalek1.getRow(), dalek1.getCol());
+            }
+            if(dalek2.hasCrashed() == true){
+                board.putPeg(Color.RED, dalek2.getRow(), dalek2.getCol());
+            }
+            if(dalek3.hasCrashed() == true){
+                board.putPeg(Color.RED, dalek3.getRow(), dalek3.getCol());
+            }
+            
+        }
+    }
+}
